4e2b51f85539996b8e7f3e949a7ede0bbf8ff08b
meacoding <16675876+meacoding@users.noreply.github.com>
2024-03-09 15:59:52 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+            <h1 className="display-4">Tax Accrual</h1>
+            <hr className="my-4" />
       </header>
     </div>
   );
