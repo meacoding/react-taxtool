b36b6647c2a75cc6a32abc9046773d20ceb069ca
meacoding <16675876+meacoding@users.noreply.github.com>
2024-03-09 16:37:17 -0500

    install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="container">
-      <header className="App-header">
+      <header>
             <h1 className="display-4">Tax Accrual</h1>
             <hr className="my-4" />
       </header>
