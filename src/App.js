efa3196c97eaa633bea05d24db26d4791dc440d5
meacoding <16675876+meacoding@users.noreply.github.com>
2024-03-17 12:07:12 -0400

    layout updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './App.css';
+import Calc from './Calc';
+import Card from './Card';
 
 function App() {
   return (
     <div className="container">
       <header>
-            <h1 className="display-4">Tax Accrual</h1>
+            <h1 className="display-3"><strong>Tax Accrual</strong></h1>
             <hr className="my-4" />
       </header>
+      <Container>
+      <Row>
+        <Col sm={4}>
+          <h2 className="display-6 mb-4">&lt;Calc /&gt;</h2>
+          <Calc />
+        </Col>
+        <Col sm={8}>
+        <h2 className="display-6 mb-4">&lt;Card /&gt;</h2>
+          <Card />
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 }
